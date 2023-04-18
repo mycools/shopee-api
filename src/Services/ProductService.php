fe86568c3c037e70c98e2492fde8756c7ae1646e
Jirawat Rojwaratkul <admin@mycools.in.th>
2023-04-18 18:28:03 +0000

    Add Item Info

--- src/Services/ProductService.php
+++ src/Services/ProductService.php
@@ -36,4 +36,65 @@ class ProductService extends BaseService
 
         return null;
     }
+    public function getItemBaseInfo(int $shop_id, array $item_id_list = [])
+    {
+        $shop = ShopeeShop::findOrFail($shop_id);
+
+        $partner_id = app('shopee')->getPartnerId();
+        $route = 'product.get_item_base_info';
+        $path = app('shopee')->getPath($route);
+        $access_token = data_get($shop, 'accessToken.access_token');
+        $signature = app('shopee')->helper()->generateSignature($path, [$access_token, $shop_id]);
+
+        $query_string = [
+            'partner_id' => $partner_id,
+            'timestamp' => $signature['time'],
+            'access_token' => $access_token,
+            'shop_id' => $shop_id,
+            'sign' => $signature['signature'],
+            'item_id_list' => $item_id_list
+        ];
+
+        $response = $this->route($route)
+            ->queryString($query_string)
+            ->payload($params)
+            ->execute();
+
+        if ($response) {
+            return data_get($response, 'response');
+        }
+
+        return null;
+    }
+    public function getItemExtraInfo(int $shop_id, array $item_id_list = [])
+    {
+        $shop = ShopeeShop::findOrFail($shop_id);
+
+        $partner_id = app('shopee')->getPartnerId();
+        $route = 'product.get_item_extra_info';
+        $path = app('shopee')->getPath($route);
+        $access_token = data_get($shop, 'accessToken.access_token');
+        $signature = app('shopee')->helper()->generateSignature($path, [$access_token, $shop_id]);
+
+        $query_string = [
+            'partner_id' => $partner_id,
+            'timestamp' => $signature['time'],
+            'access_token' => $access_token,
+            'shop_id' => $shop_id,
+            'sign' => $signature['signature'],
+            'item_id_list' => $item_id_list
+        ];
+
+        $response = $this->route($route)
+            ->queryString($query_string)
+            ->payload($params)
+            ->execute();
+
+        if ($response) {
+            return data_get($response, 'response');
+        }
+
+        return null;
+    }
+    
 }
